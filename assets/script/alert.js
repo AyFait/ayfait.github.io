3b1ba6d3205dc990439b3eef6db1bec727c2e656
Ayobami Jr. <50885913+AyFait@users.noreply.github.com>
2024-08-17 13:08:52 +0100

    Update alert.js

--- assets/script/alert.js
+++ assets/script/alert.js
@@ -1,1 +1,13 @@
+document.getElementById('bio-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    document.getElementById('popup').style.display = 'flex';
+});
 
+document.getElementById('enter-bio').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+    window.location.href = '#bio';
+});
+
+document.getElementById('close-popup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+});
