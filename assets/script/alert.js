7d566f78104c5ea00a8ddd2745464686aa06813e
Ayobami Jr. <50885913+AyFait@users.noreply.github.com>
2024-08-17 15:53:24 +0100

    Update alert.js

--- assets/script/alert.js
+++ assets/script/alert.js
@@ -14,16 +14,27 @@ document.getElementById('close-popup').addEventListener('click', function() {
 */
 
 
-document.getElementById('bio-link').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default link behavior
-    document.getElementById('popup').style.display = 'flex';
-});
+// wiki tab element
+const wikiTab = document.getElementById('wiki-tab');
 
-document.getElementById('enter-bio').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'none';
-    window.location.href = 'wiki.html'; // Navigate to wiki.html
-});
+// click event listener to the wiki tab
+wikiTab.addEventListener('click', (e) => {
+    // Prevent the default link behavior
+    e.preventDefault();
 
-document.getElementById('close-popup').addEventListener('click', function() {
-    document.getElementById('popup').style.display = 'none';
+    // Display the popup
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex';
+
+    // event listeners to the popup buttons
+    document.getElementById('proceed-btn').addEventListener('click', () => {
+        // Hide the popup and navigate to the wiki page
+        popup.style.display = 'none';
+        window.location.href = 'wiki.html';
+    });
+
+    document.getElementById('go-back-btn').addEventListener('click', () => {
+        // Hide the popup
+        popup.style.display = 'none';
+    });
 });
