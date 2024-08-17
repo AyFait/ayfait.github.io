744061e1ca511c6940a6903e2f839110efcc1894
Ayobami Jr. <50885913+AyFait@users.noreply.github.com>
2024-08-17 15:15:12 +0100

    Update alert.js

--- assets/script/alert.js
+++ assets/script/alert.js
@@ -1,4 +1,4 @@
-document.getElementById('bio-link').addEventListener('click', function(event) {
+/*document.getElementById('bio-link').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default link behavior
     document.getElementById('popup').style.display = 'flex';
 });
@@ -6,6 +6,22 @@ document.getElementById('bio-link').addEventListener('click', function(event) {
 document.getElementById('enter-bio').addEventListener('click', function() {
     document.getElementById('popup').style.display = 'none';
     window.location.href = '#bio';
+});
+
+document.getElementById('close-popup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+});
+*/
+
+
+document.getElementById('bio-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    document.getElementById('popup').style.display = 'flex';
+});
+
+document.getElementById('enter-bio').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+    window.location.href = 'wiki.html'; // Navigate to wiki.html
 });
 
 document.getElementById('close-popup').addEventListener('click', function() {
